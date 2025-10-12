235abc71cede23adff9729dc63bbb096efa372d7
Omar-bit <Bouassidaomar9@gmail.com>
2025-10-12 18:21:39 +0100

    fix: export BE URL to .env

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { showError, TOAST_MESSAGES } from '../lib/toast';
 
-const API_BASE_URL = 'http://localhost:3000';
+const API_BASE_URL =
+  import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000';
 
 export const api = axios.create({
   baseURL: API_BASE_URL,
